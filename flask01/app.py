58787c69ee2baec961f45dd418a0ce5edfa79adf
Karen Yoval <karenyoval26@gmail.com>
2025-03-07 10:38:41 -0600

    Rutas en flask

--- flask01/app.py
+++ flask01/app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
-def hola_mundo():
-    return "Hola Mundo!!"
+
+@app.route('/inicio')
+def inicio():
+    return '<h1 style="color:red;">Inicioo!!</h1>'
+
+@app.route('/adios')
+def adios():
+    return '<h1 style="color:blue;">Adios!!</h1>'
+
+@app.route('/json')
+def algo():
+    return {"nombre":"Juan", "edad":30}
+
+@app.route('/XML')
+def XML():
+   return "<nombre>Karen</nombre><edad>20</edad>"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0'
-    debug=True
-    )
+    app.run(host='0.0.0.0', debug=True)
+
