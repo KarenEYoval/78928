98c43b04c27a9719b8c54374976f5dc461d6c184
Karen Yoval <karenyoval26@gmail.com>
2025-04-24 10:21:30 -0600

    Express

--- flask01/app.py
+++ flask01/app.py
@@ -1,23 +1,41 @@
-from flask import Flask
+#Importar lo necesario para que flask funcione
+#Importar la clase Flask
+#Response es para devolver una respuesta en formato JSON
+from flask import Flask, Response 
 
+#Creamos la aplicacion y le damos un nombre
 app = Flask(__name__)
 
-@app.route('/inicio')
+#Es un decorador. Esto le dice a Flask que cada vez que un usuario visite la URL /, 
+#se debe ejecutar la función que está justo debajo de esa línea. 
+#En este caso, es la función inicio().
+@app.route('/')
 def inicio():
-    return '<h1 style="color:red;">Inicioo!!</h1>'
+    return 'Bienvenido a la pagina de inicio!'
+
+@app.route('/saludar')
+def hola_mundo():
+    return 'Hola Mundo!'
+
+@app.route('/hola')
+def hola_html():
+    return '<h1 style="color:red";>Hola!</h1> '
 
 @app.route('/adios')
-def adios():
-    return '<h1 style="color:blue;">Adios!!</h1>'
+def adios_mundo():
+    return 'Adios Mundo!'
 
 @app.route('/json')
 def algo():
-    return {"nombre":"Juan", "edad":30}
+    return Response('{"nombre":"John"}', mimetype='application/json')
 
-@app.route('/XML')
-def XML():
-   return "<nombre>Karen</nombre><edad>20</edad>"
+@app.route('/xml')
+def xml():
+    xml= '''<?xml version="1.0"?>
+    <persona>
+    <nombre>John</nombre>
+    </persona>'''
+    return Response(xml, mimetype='application/xml')
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', debug=True)
-
--- flask02/rutas.py
+++ flask02/rutas.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-from modelos import Producto  # Make sure you have the Producto class in modelos module
+from modelos import Producto  
+#Es una función que te permite renderizar archivos HTML 
+#Usamos esto cuando queremos pasarle datos a un archivo HTML y mostrarlo en el navegador
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    productos = [Producto("Manzana", 12), Producto("Peras", 13)]  # Assuming Producto takes (name, price)
+    productos = [Producto("Manzana", 12), Producto("Peras", 13)]  
+    # Assuming Producto takes (name, price)
     return render_template('index.html', productos=productos)
 
 if __name__ == '__main__':
